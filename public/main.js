e3dd6070b29cbef8565c18422b2bde7ac719f5c0
Shantell Smithson <perseus.ripple@gmail.com>
2019-02-10 09:34:04 -0500

    added get sum function

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants'
+
 /*const main = () => {
   if (document.querySelector('h1.hello-world')) {
     document.querySelector('h1.hello-world').textContent = 'Hello, World!'
@@ -8,40 +10,33 @@ document.addEventListener('DOMContentLoaded', main)
 
 let deck = []
 
-const cardsNumber = [
-  
+const cardsValue = [
   '2',
-  
   '3',
-  
   '4',
-  
   '5',
-  
   '6',
-  
   '7',
-  
   '8',
-  
   '9',
-  
   '10',
-  
   'jack',
-  
   'queen',
-  
   'king',
-  
   'ace'
 ]
 
 const cardsSuit = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 
+let numOfCardsPlayer = 0
+
+let numOfCardsDealer = 0
+
+let numOfWins = 0
+
 /*let deck = []
 
-let numberofcardsInDeck = 65
+let numberOfCardsInDeck = 65
 cardsInDeck[0] = '2_of_clubs'
 cardsInDeck[1] = '2_of_diamonds'
 cardsInDeck[2] = '2_of_hearts'
@@ -49,16 +44,65 @@ cardsInDeck[3] = '2_of_spades'
 cardsInDeck[4] = '3_of_clubs' */
 
 const main = () => {
-  // call function to get card value
-  getCardValue()
   // call function to create deck on page load
   buildDeck()
   // call function
   shuffleDeck()
 }
 
-// Get Card Value
+// Build New Deck
 
+const buildDeck = () => {
+  for (let i = 0; i < cardsSuit.length; i++) {
+    // console.log(deck)
+    for (let j = 0; j < cardsValue.length; j++) {
+      // let value = cardsValue[value]
+      // let suit = cardsSuit[suits]
+      /*deck.push('the ' + valuesOfCards[j] + ' of ' + suitsOfCards[i])) */
+      //console.log(deck push)
+    }
+  }
+
+  // Display Cards
+
+  const displayCards = () => {
+    for (let i = 0; i < deck.length; i++) {
+      let imgElement = document.createElement('img')
+      imgElement.src =
+        'images/' + deck[i].cardsValue + '_of_' + deck[i].cardSuit + '.svg'
+      imgElement.style.height = '120px'
+      imgElement.style.width = '100px'
+      if (displayCards === mainPlayer) {
+        numberOfCardsPlayer++
+        player1Hand.appendChild(imgElement)
+      } else {
+        numberOfCardsDealer++
+        dealerHand.appendChild(imgElement)
+      }
+    }
+  }
+
+  // Get Card Sum by adding numeric values of dealt cards
+
+  let sum = 0
+  let aces = 0
+  for (let i = 0; i < deck.length; i++) {
+    if (deck[i].getCardValue() === 11) {
+      aces += 1
+      sum = sum + deck[i].getCardValue()
+    } else {
+      sum = sum + deck[i].getCardValue()
+    }
+  }
+  while (aces > 0 && sum > 21) {
+    aces -= 1
+    sum -= 10
+  }
+  return sum
+}
+
+// Get Card Value
+/*
 const getCardValue = () => {
   if (cardNumber === 'jack' || cardNumber === 'queen' || cardNumber === 'king') {
     return 10
@@ -67,20 +111,7 @@ const getCardValue = () => {
 } else {
   return cardNumber
 }
-}
-
-// Build Deck
-
-const buildDeck = () => {
-  for (let i = 0; i < cardsSuit.length; i++) {
-    // console.log(deck)
-    for (let j = 0; j < cardsNumber.length; j++) {
-      // let num = cardsNumber[num]
-      // let suit = cardsSuit[suits]
-      deck.push('images/' + cardsNumber[j] + ' of ' + cardsSuit[i] + '.svg')
-  //console.log(deck push)    
-  }
-}
+} */
 
 console.log(deck)
 const main = () => {
@@ -112,9 +143,7 @@ const shuffleDeck = () => {
 // Get Values
 
 const getValues = () => {
-for (let i = deck.length; counter = 0; count_1 = false){
-
-}
+  for (let i = deck.length; (counter = 0); count_1 = false) {}
 }
 
 // Deal Card
@@ -128,7 +157,6 @@ const dealCard = () => {
   document.removeCard 
 } */
 
-
 // console.log(deck)
 
 // Deal Card (org)
@@ -141,14 +169,13 @@ const dealCard = () => {
   }
   let nextNewCard = deck.splice(0, 1)
 
-
   //from original shuffle assignment
   if (deck.length > 0) {
     // const _li = document.createElement('li')
     // li.textContent = newCard
     document.querySelector('.player-1').textContent = nextNewCard
   }
-} 
+}
 
 // console.log('return')
 /*
